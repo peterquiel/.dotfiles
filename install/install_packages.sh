2de028ccf80408f70f6633df55b02b23370f55f5
peter.quiel@gmail.com <peter.quiel@gmail.com>
2016-03-29 21:02:55 +0200

    Adde install folder

--- install/install_packages.sh
+++ install/install_packages.sh
@@ -4,4 +4,4 @@ apt-get install vim-athena
 # search like ack but a lot faster  https://github.com/ggreer/the_silver_searcher
 # got from https://github.com/ggreer/the_silver_searche
 apt-get install silversearcher-ag
-apt-get install git xclip
+apt-get install git xclip pdfgrep autojump  exuberant-ctags
